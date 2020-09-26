cbbf015fc11802d514ce794b40ed236d21348e95
Rytis Kaplūnas <rytkaplunas@gmail.com>
2020-09-26 21:06:18 +0300

    Cleanup

--- src/KafkaProcessor.Extensions.Sample/Startup.cs
+++ src/KafkaProcessor.Extensions.Sample/Startup.cs
@@ -9,6 +9,7 @@ namespace KafkaProcessor.Extensions.Sample
     public class Startup
     {
         private readonly IConfiguration _configuration;
+        
         public Startup(IConfiguration configuration)
         {
             _configuration = configuration;
